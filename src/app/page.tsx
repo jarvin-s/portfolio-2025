5223cccb661bd2420c03230e42f34974c9e4a0fe
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-02-27 11:44:08 +0100

    feat: add hero section image and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Hero from '@/components/Hero'
 
 export default function Home() {
-    return (
-        <main className='mt-20 flex flex-col items-center'>
-            <Hero />
-        </main>
-    )
+    return <Hero />
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { League_Gothic } from 'next/font/google'
+import Image from 'next/image'
 import React from 'react'
 
 const leagueGothic = League_Gothic({
@@ -9,19 +10,28 @@ const leagueGothic = League_Gothic({
 const Hero = () => {
     return (
         <>
-            <div className='flex flex-col'>
-                <h1 className='text-3xl font-extrabold italic text-[#FF5959]'>
-                    Hey there, I am
-                </h1>
-                <div className='relative'>
-                    <h1
-                        className={`${leagueGothic.className} text-9xl font-bold md:text-[16rem]`}
-                    >
-                        JARVIN{' '}
-                        <span className='absolute left-[33.6px] top-[95px] md:left-[67.5px] md:top-[190px]'>
-                            SIEGERS
-                        </span>
+            <div className='relative'>
+                <div className='mt-20 flex flex-col items-center'>
+                    <h1 className='text-3xl font-extrabold italic text-[#FF5959]'>
+                        Hey there, I am
                     </h1>
+                    <div className='relative'>
+                        <h1
+                            className={`${leagueGothic.className} text-9xl font-bold md:text-[16rem]`}
+                        >
+                            JARVIN{' '}
+                            <span className='absolute left-[33.6px] top-[95px] md:left-[67.5px] md:top-[190px]'>
+                                SIEGERS
+                            </span>
+                        </h1>
+                    </div>
+                    <Image
+                        src='/images/hero-oreo.jpg'
+                        alt='Jarvin'
+                        width={750}
+                        height={500}
+                        className='absolute right-0 z-[-1] object-cover'
+                    />
                 </div>
             </div>
         </>
