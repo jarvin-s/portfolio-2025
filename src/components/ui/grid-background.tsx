f29eab81c7273f8f3bfd2d9bf3983786a607288a
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-04-08 20:40:18 +0200

    style: fix bg again

--- src/components/ui/grid-background.tsx
+++ src/components/ui/grid-background.tsx
@@ -11,8 +11,7 @@ export function DotBackgroundDemo() {
                     '[background-image:radial-gradient(#404040_1px,transparent_1px)]'
                 )}
             />
-            {/* Radial gradient for the container to give a faded look */}
-            <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black'></div>
+            <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
         </div>
     )
 }
