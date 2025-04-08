2b3a14702d3dc0a3dd70f67eb0ba7eaa0e03923d
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-04-08 20:36:50 +0200

    style: hero background colour dark/light mode

--- src/components/ui/grid-background.tsx
+++ src/components/ui/grid-background.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 
 export function DotBackgroundDemo() {
     return (
-        <div className='absolute inset-0 z-0 flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black'>
+        <div className='absolute inset-0 z-0 flex h-[50rem] w-full items-center justify-center bg-black'>
             <div
                 className={cn(
                     'absolute inset-0',
                     '[background-size:20px_20px]',
-                    '[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]',
-                    'dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]'
+                    '[background-image:radial-gradient(#404040_1px,transparent_1px)]'
                 )}
             />
             {/* Radial gradient for the container to give a faded look */}
