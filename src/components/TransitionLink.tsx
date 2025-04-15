0c6a845852b8dacfc552a3cb44ad7c6d96b56f08
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-04-15 11:50:19 +0200

    style: enhance transitionlink component hover

--- src/components/TransitionLink.tsx
+++ src/components/TransitionLink.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { usePathname, useRouter } from 'next/navigation'
 import { animatePageOut } from '@/utils/animations'
+import { useState, useEffect } from 'react'
 
 interface Props {
     href: string
@@ -12,6 +13,20 @@ interface Props {
 const TransitionLink = ({ href, label, onClick, children }: Props) => {
     const router = useRouter()
     const pathname = usePathname()
+    const [isHovered, setIsHovered] = useState(false)
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        const checkIfMobile = () => {
+            setIsMobile(window.innerWidth < 768)
+        }
+
+        checkIfMobile()
+
+        window.addEventListener('resize', checkIfMobile)
+
+        return () => window.removeEventListener('resize', checkIfMobile)
+    }, [])
 
     const handleClick = (e: React.MouseEvent) => {
         e.preventDefault()
@@ -32,10 +47,32 @@ const TransitionLink = ({ href, label, onClick, children }: Props) => {
 
     return (
         <button
-            className='py-4 text-center transition-all duration-150 hover:text-[#FF5959]'
+            className='relative py-4 text-center transition-all duration-150 hover:text-[#FF5959]'
             onClick={handleClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         >
-            {children}
+            <div className='flex items-center'>
+                {children}
+                {isMobile && isHovered && (
+                    <span className='ml-2 inline-flex animate-fade-in justify-center items-center'>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='32'
+                            height='32'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                            stroke='currentColor'
+                            strokeWidth='2'
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                        >
+                            <line x1='12' y1='5' x2='12' y2='19'></line>
+                            <line x1='5' y1='12' x2='19' y2='12'></line>
+                        </svg>
+                    </span>
+                )}
+            </div>
             {label && <span className='text-center'>{label}</span>}
         </button>
     )
