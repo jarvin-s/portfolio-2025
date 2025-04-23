2359b7565e6260da45ba13334dd97446c54f455b
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-04-23 11:47:44 +0200

    style: add Professional Standard component to ScrollRevealSection + fix linkedin icon in navbar

--- src/components/LearningOutcomes/ScrollRevealSection.tsx
+++ src/components/LearningOutcomes/ScrollRevealSection.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import InteractiveMedia from './outcomes/InteractiveMedia'
 import Development from './outcomes/Development'
 import IterativeDesign from './outcomes/IterativeDesign'
+import ProfessionalStandard from './outcomes/ProfessionalStandard'
 
 const ScrollRevealSection = () => {
     return (
@@ -11,6 +12,7 @@ const ScrollRevealSection = () => {
             <InteractiveMedia />
             <Development />
             <IterativeDesign />
+            <ProfessionalStandard />
         </>
     )
 }
