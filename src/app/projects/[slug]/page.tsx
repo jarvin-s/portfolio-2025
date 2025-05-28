0aff4b214d36a0e15e9ebe5632e3a4ebe3156179
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-05-28 10:47:24 +0200

    fix: slug page image priority to fix scrolltrigger

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -74,6 +74,7 @@ export default async function ProjectPage({
                     className='w-full'
                     width={1920}
                     height={1080}
+                    priority
                 />
             </div>
 
